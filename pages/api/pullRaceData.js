c40921874b3b5da97cafbb0749e0968040808a43
Christy Campbell <christytc10@gmail.com>
2021-04-21 21:10:03 +0100

    Check if netlify supports a tmp directory in the app dir

---
+++ pages/api/pullRaceData.js
@@ -0,0 +1,37 @@
+const ftp = require("basic-ftp");
+const path = require("path");
+const fs = require("fs");
+
+const client = new ftp.Client();
+client.ftp.verbose = true;
+
+const tmpDir = process.env.RaceDataTmpPath || ".tmpRaceData/";
+
+export default async function handler(req, res) {
+  const results = await GetResults();
+  const files = results.join(',')
+  res.end(files);
+}
+
+async function GetResults() {
+  if (!fs.existsSync(tmpDir)) {
+    fs.mkdirSync(tmpDir);
+  }
+
+  try {
+    await client.access({
+      host: process.env.RaceDataFtpHost,
+      port: process.env.RaceDataFtpPort,
+      user: process.env.RaceDataFtpUsername,
+      password: process.env.RaceDataFtpPassword,
+    });
+    await client.downloadToDir(tmpDir, process.env.RaceDataFtpRemoteDir);
+    const files = fs.readdirSync(tmpDir);
+    console.log(files)
+    
+    return files;
+  } catch (err) {
+    console.log(err);
+  }
+  client.close();
+}
