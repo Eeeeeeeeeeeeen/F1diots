b92890b7ee08b6ce94847b60efba3077f3f0b5ca
Ian <ianthomson92@gmail.com>
2021-04-28 12:41:09 +0100

    Added tabs

--- pages/race/[id].js
+++ pages/race/[id].js
@@ -1,5 +1,6 @@
 import { Container, Heading } from "@chakra-ui/layout";
 import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/tabs";
 import { calulateLapTime } from "../../utils/timeFormatter";
 
 export async function getServerSideProps(context) {
@@ -25,37 +26,48 @@ export default function Race({ raceData }) {
 
   return (
     <Container maxW="750px" mt="20px">
-      {drivers.map((driver) => (
-        <>
-          <Heading as="h2" size="lg">
-            {`${driver[0]} - ${driver[1].carDetails.name}`}
-          </Heading>
-          <Table mb="40px">
-            <Thead>
-              <Tr>
-                <Th>LapTime</Th>
-                <Th>Split 1</Th>
-                <Th>Split 2</Th>
-                <Th>Split 3</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {driver[2].map((l) => (
-                <Tr
-                  key={l.laptime}
-                  backgroundColor={l.laptime === driver[3] ? "orange.800" : ""}
-                >
-                  <Td fontWeight="bold">{calulateLapTime(l.laptime)}</Td>
+      <Tabs mb="20px">
+        <TabList>
+          {drivers.map((driver) => (
+            <Tab>{driver[0]}</Tab>
+          ))}
+        </TabList>
+        <TabPanels>
+          {drivers.map((driver) => (
+            <TabPanel>
+              <Heading as="h2" size="lg">
+                {`${driver[0]} - ${driver[1].carDetails.name}`}
+              </Heading>
+              <Table mb="40px">
+                <Thead>
+                  <Tr>
+                    <Th>LapTime</Th>
+                    <Th>Split 1</Th>
+                    <Th>Split 2</Th>
+                    <Th>Split 3</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {driver[2].map((l) => (
+                    <Tr
+                      key={l.laptime}
+                      backgroundColor={
+                        l.laptime === driver[3] ? "orange.800" : ""
+                      }
+                    >
+                      <Td fontWeight="bold">{calulateLapTime(l.laptime)}</Td>
 
-                  {l.splits.map((split) => (
-                    <Td>{calulateLapTime(split)}</Td>
+                      {l.splits.map((split) => (
+                        <Td>{calulateLapTime(split)}</Td>
+                      ))}
+                    </Tr>
                   ))}
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
-        </>
-      ))}
+                </Tbody>
+              </Table>
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
     </Container>
   );
 }
